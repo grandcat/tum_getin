30a5e8d2143a92fd897a2a373294d3b321b12d80
Stefan S <smarzlys@gmail.com>
2015-06-14 21:22:46 +0200

    Refactoring: different files for each responsability

---
+++ code/nfcCommunicator/PyNfcMessaging/nfc_reader.py
@@ -0,0 +1,190 @@
+"""Simple NFC card emulation"""
+
+import logging
+import ctypes
+
+import nfc
+from nfc_error import HWError
+from nfc_statemachine import CommStateMachine
+
+def hex_dump(buffer):
+    """Dumps the buffer as an hex string"""
+    return ' '.join(["%0.2X" % x for x in buffer])
+
+# NFC device setup
+class NFCReader(object):
+    """Short APDU max transceive length: ISO7816_SHORT_APDU_MAX_DATA_LEN + APDU_COMMAND_HEADER + APDU_RESPONSE_TRAILER """
+    ISO7816_SHORT_APDU_MAX_LEN = 256 + 4 + 2
+
+    def __init__(self, log=None):
+        self.log = log or logging.getLogger(__name__)
+        # NFC hardware fields
+        self.__context = None
+        self.__device = None
+        self.__target = None
+        # Send and receive buffer for exchanging one APDU message over NFC
+        self.__tx_msg = (ctypes.c_uint8 * self.ISO7816_SHORT_APDU_MAX_LEN)()
+        self.__rx_msg = (ctypes.c_uint8 * self.ISO7816_SHORT_APDU_MAX_LEN)()
+
+        self._card_present = False
+        self._card_last_seen = None
+        self._card_uid = None
+        self._clean_card()
+
+        # Init
+        self.init_card_emulation_target()
+
+    @staticmethod
+    def _sanitize(bytesin):
+        """Returns guaranteed ascii text from the input bytes"""
+        return "".join([x if 0x7f > ord(x) > 0x1f else '.' for x in bytesin])
+
+
+    @staticmethod
+    def _hashsanitize(bytesin):
+        """Returns guaranteed hexadecimal digits from the input bytes"""
+        return "".join([x if x.lower() in 'abcdef0123456789' else '' for x in bytesin])
+
+
+    def init_card_emulation_target(self):
+        """
+        Prepares NFC reader hardware for card emulation
+        """
+        if self.__target == None:
+            self.__target = nfc.nfc_target()
+
+        # Modulation properties
+        modulation = self.__target.nm       # access by reference
+        modulation.nmt = nfc.NMT_ISO14443A
+        modulation.nbr = nfc.NBR_UNDEFINED
+
+        # Target info: card emulation
+        card = nfc.nfc_iso14443a_info() # type: target_info
+        card.abtAtqa[:] = [0x00, 0x04]
+        #atqaSet = (ctypes.c_uint8 * 2).from_buffer(card.abtAtqa)
+        #atqaSet[:] = [0x00, 0x04]
+        # card.abtAtqa[:] = [0x00, 0x04] # also seems to work directly
+        print(card.abtAtqa[:])
+
+        uid = "\x08\x00\xb0\xfe"
+        for i in range(4):
+            card.abtUid[i] = ord(uid[i])
+        card.szUidLen = 4
+        #card.abtAtqa = [0x00, 0x04]
+
+        card.btSak = 0x20
+
+        atsSet = (ctypes.c_uint8 * 4).from_buffer(card.abtAts)
+        atsSet[:] = [0x75, 0x33, 0x92, 0x03]
+        print(card.abtAts[:])
+        card.szAtsLen = 4
+
+        # Assign target_info for card emulation
+        self.__target.nti.nai = card
+
+
+    def run(self):
+        """
+        Connects with the NFC reader and starts messaging with NFC devices
+        """
+        self.__context = ctypes.pointer(nfc.nfc_context())
+        self.log.debug('NFC init')
+        nfc.nfc_init(ctypes.byref(self.__context))
+        connection_loop = True
+        try:
+            self._clean_card()
+            conn_strings = (nfc.nfc_connstring * 1)()
+            devices_found = nfc.nfc_list_devices(self.__context, ctypes.byref(conn_strings), 1)
+            if devices_found != 1:
+                raise HWError('No NFC device found. Check your libnfc config and hardware.')
+
+            self.log.debug('NFC open')
+            # NFC abstraction: open
+            self.__device = nfc.nfc_open(self.__context, conn_strings[0])
+            self.log.debug('NFC_open finished')
+            # self.log.debug(self.__device.last_error)
+
+            while connection_loop:
+                # Reset all state variables
+                self.log.debug('Wait for NFC initiator for 5s')
+                # NFC abstraction: target_init
+                connection_res = nfc.nfc_target_init(self.__device,
+                                                     ctypes.byref(self.__target),
+                                                     ctypes.pointer(self.__rx_msg),
+                                                     self.ISO7816_SHORT_APDU_MAX_LEN, 5000)
+                print(connection_res)
+                if connection_res >= 0:
+                    self.log.debug(self.__rx_msg[:])
+
+                    # Start message exchange state machine
+                    self._message_loop()
+
+                elif connection_res != nfc.NFC_ETIMEOUT:
+                    self.log.warning('nfc_target_init: got error %i', connection_res)
+
+        except (KeyboardInterrupt, SystemExit):
+            connection_loop = False
+        except HWError as e:
+            self.log.error("Hardware exception: " + str(e))
+            connection_loop = False
+        except IOError as e:
+            self.log.error("IOError Exception: " + str(e))
+            connection_loop = True
+
+        finally:
+            nfc.nfc_close(self.__device)
+            nfc.nfc_exit(self.__context)
+            self.log.info('NFC clean shutdown')
+        return connection_loop
+
+
+    def _message_loop(self):
+        """Starts a loop that simulates a smartcard"""
+        stm = CommStateMachine()
+
+        loop = True
+        while loop:
+            # Receive APDU message
+            print("Receive first string")
+            rx_len = nfc.nfc_target_receive_bytes(self.__device,
+                                                  ctypes.byref(self.__rx_msg),
+                                                  self.ISO7816_SHORT_APDU_MAX_LEN, 0)
+            if (rx_len < 0):
+                # Error occurred (e.g., target was removed from the carrier field) --> restart
+                self.log.warning('Error %d in nfc_target_receive_bytes', rx_len)
+                return
+
+            rx_buf = bytes((ctypes.c_char * rx_len).from_buffer(self.__rx_msg))
+            self.log.debug('Receive [%d bytes]: %s', rx_len, hex_dump(rx_buf[:]))
+
+            # State machine for communication with end device
+            tx_buf, tx_len = stm.do_IO(rx_buf, rx_len)
+            loop = not stm.should_stop()
+            # time.sleep(1) # testing tolerance of timeout value
+
+            # Transmit message
+            if tx_len > 0:
+                # TODO: improve python->ctype conversion
+                self.__tx_msg[:tx_len] = tx_buf
+                tx_len = nfc.nfc_target_send_bytes(self.__device,
+                                                   ctypes.byref(self.__tx_msg),
+                                                   tx_len, 0)
+                self.log.debug('Send [%d bytes]: %s', tx_len, hex_dump(tx_buf[:]))
+                if (tx_len < 0):
+                    # Error occurred (e.g., target was removed from the carrier field) --> restart
+                    self.log.warning('Error %d in nfc_target_send_bytes', tx_len)
+                    return
+
+            # loop = False
+
+    def _clean_card(self):
+        self._card_uid = None
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    reader = NFCReader(logger)
+    while reader.run():
+        pass
--- code/nfcCommunicator/PyNfcMessaging/nfc_statemachine.py
+++ code/nfcCommunicator/PyNfcMessaging/nfc_statemachine.py
@@ -1,198 +1,15 @@
-"""Simple NFC card emulation"""
-
 import logging
 import struct
-import time
-
-from nfc_error import HWError
-import ctypes
-import nfc
-
-def hex_dump(buffer):
-    """Dumps the buffer as an hex string"""
-    return ' '.join(["%0.2X" % x for x in buffer])
-
-# NFC device setup
-class NFCReader(object):
-    """Short APDU max transceive length: ISO7816_SHORT_APDU_MAX_DATA_LEN + APDU_COMMAND_HEADER + APDU_RESPONSE_TRAILER """
-    ISO7816_SHORT_APDU_MAX_LEN = 256 + 4 + 2
-
-    def __init__(self, log=None):
-        self.log = log or logging.getLogger(__name__)
-        # NFC hardware fields
-        self.__context = None
-        self.__device = None
-        self.__target = None
-        # Send and receive buffer for exchanging one APDU message over NFC
-        self.__tx_msg = (ctypes.c_uint8 * self.ISO7816_SHORT_APDU_MAX_LEN)()
-        self.__rx_msg = (ctypes.c_uint8 * self.ISO7816_SHORT_APDU_MAX_LEN)()
-
-        self._card_present = False
-        self._card_last_seen = None
-        self._card_uid = None
-        self._clean_card()
-
-        # Init
-        self.init_card_emulation_target()
-
-    @staticmethod
-    def _sanitize(bytesin):
-        """Returns guaranteed ascii text from the input bytes"""
-        return "".join([x if 0x7f > ord(x) > 0x1f else '.' for x in bytesin])
-
-
-    @staticmethod
-    def _hashsanitize(bytesin):
-        """Returns guaranteed hexadecimal digits from the input bytes"""
-        return "".join([x if x.lower() in 'abcdef0123456789' else '' for x in bytesin])
-
-
-    def init_card_emulation_target(self):
-        """
-        Prepares NFC reader hardware for card emulation
-        """
-        if self.__target == None:
-            self.__target = nfc.nfc_target()
-
-        # Modulation properties
-        modulation = self.__target.nm       # access by reference
-        modulation.nmt = nfc.NMT_ISO14443A
-        modulation.nbr = nfc.NBR_UNDEFINED
-
-        # Target info: card emulation
-        card = nfc.nfc_iso14443a_info() # type: target_info
-        card.abtAtqa[:] = [0x00, 0x04]
-        #atqaSet = (ctypes.c_uint8 * 2).from_buffer(card.abtAtqa)
-        #atqaSet[:] = [0x00, 0x04]
-        # card.abtAtqa[:] = [0x00, 0x04] # also seems to work directly
-        print(card.abtAtqa[:])
-
-        uid = "\x08\x00\xb0\xfe"
-        for i in range(4):
-            card.abtUid[i] = ord(uid[i])
-        card.szUidLen = 4
-        #card.abtAtqa = [0x00, 0x04]
-
-        card.btSak = 0x20
-
-        atsSet = (ctypes.c_uint8 * 4).from_buffer(card.abtAts)
-        atsSet[:] = [0x75, 0x33, 0x92, 0x03]
-        print(card.abtAts[:])
-        card.szAtsLen = 4
-
-        # Assign target_info for card emulation
-        # self.__target.nm = modulation
-        #self.__target.nti = self.__target_info
-        self.__target.nti.nai = card
-
-
-    def run(self):
-        """
-        Connects with the NFC reader and starts messaging with NFC devices
-        """
-        self.__context = ctypes.pointer(nfc.nfc_context())
-        self.log.debug('NFC init')
-        nfc.nfc_init(ctypes.byref(self.__context))
-        connection_loop = True
-        try:
-            self._clean_card()
-            conn_strings = (nfc.nfc_connstring * 1)()
-            devices_found = nfc.nfc_list_devices(self.__context, ctypes.byref(conn_strings), 1)
-            if devices_found != 1:
-                raise HWError('No NFC device found. Check your libnfc config and hardware.')
-
-            self.log.debug('NFC open')
-            # NFC abstraction: open
-            self.__device = nfc.nfc_open(self.__context, conn_strings[0])
-            self.log.debug('NFC_open finished')
-            # self.log.debug(self.__device.last_error)
-
-            while connection_loop:
-                # Reset all state variables
-                self.log.debug('Wait for NFC initiator for 5s')
-                # NFC abstraction: target_init
-                connection_res = nfc.nfc_target_init(self.__device,
-                                                     ctypes.byref(self.__target),
-                                                     ctypes.pointer(self.__rx_msg),
-                                                     self.ISO7816_SHORT_APDU_MAX_LEN, 5000)
-                print(connection_res)
-                if connection_res >= 0:
-                    self.log.debug(self.__rx_msg[:])
-
-                    # Start message exchange state machine
-                    self._message_loop()
-
-                elif connection_res != nfc.NFC_ETIMEOUT:
-                    self.log.warning('nfc_target_init: got error %i', connection_res)
-
-        except (KeyboardInterrupt, SystemExit):
-            connection_loop = False
-        except HWError as e:
-            self.log.error("Hardware exception: " + str(e))
-            connection_loop = False
-        except IOError as e:
-            self.log.error("IOError Exception: " + str(e))
-            connection_loop = True
-
-        finally:
-            nfc.nfc_close(self.__device)
-            nfc.nfc_exit(self.__context)
-            self.log.info('NFC clean shutdown')
-        return connection_loop
-
-
-    def _message_loop(self):
-        """Starts a loop that simulates a smartcard"""
-        stm = CommStateMachine()
-
-        loop = True
-        while loop:
-            # Receive APDU message
-            print("Receive first string")
-            rx_len = nfc.nfc_target_receive_bytes(self.__device,
-                                                  ctypes.byref(self.__rx_msg),
-                                                  self.ISO7816_SHORT_APDU_MAX_LEN, 0)
-            if (rx_len < 0):
-                # Error occurred (e.g., target was removed from the carrier field) --> restart
-                self.log.warning('Error %d in nfc_target_receive_bytes', rx_len)
-                return
-
-            rx_buf = bytes((ctypes.c_char * rx_len).from_buffer(self.__rx_msg))
-            self.log.debug('Receive [%d bytes]: %s', rx_len, hex_dump(rx_buf[:]))
-
-            # State machine for communication with end device
-            tx_buf, tx_len = stm.do_IO(rx_buf, rx_len)
-            loop = not stm.should_stop()
-            # time.sleep(1) # testing tolerance of timeout value
-
-            # Transmit message
-            if tx_len > 0:
-                # TODO: improve python->ctype conversion
-                self.__tx_msg[:tx_len] = tx_buf
-                tx_len = nfc.nfc_target_send_bytes(self.__device,
-                                                   ctypes.byref(self.__tx_msg),
-                                                   tx_len, 0)
-                self.log.debug('Send [%d bytes]: %s', tx_len, hex_dump(tx_buf[:]))
-                if (tx_len < 0):
-                    # Error occurred (e.g., target was removed from the carrier field) --> restart
-                    self.log.warning('Error %d in nfc_target_send_bytes', tx_len)
-                    return
-
-            # loop = False
-
-    def _clean_card(self):
-        self._card_uid = None
-
 
 class CommStateMachine:
     IDENTIFICATION = b'tumgetin\x01' # hex: 74 75 6D 67 65 74 69 6E 01
     MAX_BYTES = 4096
-    MAX_CHUNK_SIZE = 250
+    MAX_CHUNK_SIZE = 125 # 250 quite stable, too
 
-    data_out = bytearray(b'\xbe\xef\xaf\xfe' * 128 * 2)
+    data_out = bytearray(b'\xbe\xef\xaf\xfe' * 128)
     data_out.extend([0x11, 0x22, 0x33])
     data_out_ready = False  # If true, buffer is complete and ready for transmission (NOTE: must be atomic)
-    # data_out_offset = 0
+    data_out_offset = 0     # For lazy client that doesn't want to index the data, we do it for him =)
     data_in = bytearray()
     data_in_ready = False
     data_in_size = 0
@@ -224,10 +41,6 @@ class CommStateMachine:
         tx_buffer = []
         tx_len = 0
 
-        # TODO: remove the following test
-        self.data_out_ready = True
-        self.data_in_ready = True
-
         if rx_len > 0:
             action = rx_buffer[APDU.INS]
 
@@ -238,6 +51,10 @@ class CommStateMachine:
                     # Select by name
                     if rx_buffer[APDU.DATA:] == self.IDENTIFICATION:
                         self.conn_established = True
+                        # TODO: remove the following test
+                        self.data_out_ready = True
+                        self.data_in_ready = True
+                        # END Test
                         return APDU.msg_success()
                     else:
                         # No expected client --> Goodbye
@@ -299,6 +116,12 @@ class CommStateMachine:
         buf_out = bytearray(self.data_out[offset:offset+requested_bytes])
         buf_out.extend(APDU.SUCCESS)
 
+         # Notify about finished data transfer and free for new data if end of data is reached
+        if (offset + requested_bytes) == len(self.data_out):
+            self.data_out = bytearray()
+            self.data_out_ready = False
+            self.data_out_offset = 0
+
         return buf_out, len(buf_out)
 
     def _set_data_in_size(self, rx_buffer):
@@ -310,8 +133,9 @@ class CommStateMachine:
         requested_size = struct.unpack_from('!H', rx_buffer, APDU.P2)[0]
         requested_size = min(requested_size, self.MAX_BYTES)
 
-        # Prepare data_in bytearray for merging data chunks
-        self.data_in = bytearray()
+        # Prepare buffer for merging data chunks and remove old buffer if not busy
+        self.data_in = bytearray()  # BUG: could be misused to trigger a lot of small-sized memory allocations.
+                                    #      Hopefully, GC is fast enough for cleaning up.
         self.data_in_size = requested_size
         self.log.debug('_set_data_in_size: allocating %d bytes for data input', requested_size)
         # Response: actually reserved bytes
@@ -330,10 +154,11 @@ class CommStateMachine:
         #                                 ^offset^  ^ignore as given by input_len
         offset = struct.unpack_from('!H', rx_buffer, APDU.P1)[0]
 
-        if (offset + input_len) > self.data_in_size:
+        if (offset + input_len) > self.data_in_size or input_len > self.MAX_CHUNK_SIZE:
             return APDU.msg_err_param()
 
         if (len(self.data_in) + input_len) > self.data_in_size:
+            # TODO: Clear buffer as received data is only rubbish now
             return APDU.msg_err_not_enough_space()
 
         # Insert message chunk into buffer
@@ -347,6 +172,10 @@ class CommStateMachine:
         if len(self.data_in) == self.data_in_size:
             self.data_in_ready = False
             # TODO: add notification
+            print('Finished data: %s' % self.data_in)
+            # data_in is now complete for local processing. This tells the client that all data was received
+            # successfully, but we need some time until we have data to push back to the client.
+            return APDU.msg_success_with_data_ready(0x00)
 
         return APDU.msg_success()
 
@@ -381,7 +210,7 @@ class APDU:
     @staticmethod
     def msg_success_with_data_ready(available_bytes):
         assert available_bytes < 256
-        buf = b'\x61' + bytes(available_bytes)
+        buf = b'\x61' + int.to_bytes(available_bytes, 1, byteorder='big')
         return buf, 2
 
     @staticmethod
@@ -403,12 +232,3 @@ class APDU:
     @staticmethod
     def msg_err_not_ready():
         return APDU.ERR_NOT_READY, 2
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger(__name__)
-
-    reader = NFCReader(logger)
-    while reader.run():
-        pass
