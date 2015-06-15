3872e078c2f6b773e87a9f1886e95530505f8f1e
Sebastian Schleemilch <sebastian.schleemilch@tum.de>
2015-06-15 17:57:14 +0200

    [ANDROID] bare activities for user-register interaction

---
+++ code/mobileDoorAccess/app/src/main/java/com/tca/mobiledooraccess/MainActivity.java
@@ -0,0 +1,35 @@
+package com.tca.mobiledooraccess;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+
+/**
+ * Created by basti on 15.06.15.
+ */
+public class MainActivity extends Activity {
+
+    public static final String TUM_GETIN_PREFERENCES = "TUM_GETIN_PREFS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        SharedPreferences settings = getSharedPreferences(TUM_GETIN_PREFERENCES, 0);
+        boolean registered = settings.getBoolean("registered",false);
+
+        if (registered){
+            Log.d("Registration", "The user is registered");
+            //start registered Activity
+        }else {
+            //start register Activity
+            Log.d("Registration", "The user is NOT registered");
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        }
+    }
+}
