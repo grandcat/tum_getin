989565eb67d0b6ca16954fa7a6900bb19ad730be
Sebastian Schleemilch <sebastian.schleemilch@tum.de>
2015-07-08 13:48:09 +0200

    [Android] New APP, ViewPages-based architecture, Step 1 included

---
+++ code/APP/app/src/main/java/androidpraktikum/tumgetin/MainActivity.java
@@ -0,0 +1,84 @@
+package androidpraktikum.tumgetin;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+    public static final String TUM_GETIN_PREFERENCES = "TGI_PREFS";
+    public static Context context;
+    private static final String TAG = "MainActivity";
+    FragmentPagerAdapter adapterViewPager;
+    public static ViewPager viewPager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapterViewPager);
+        MainActivity.context = getApplicationContext();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_delete_account) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 2;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        // Returns total number of pages
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: // Fragment # 0 - This will show FirstFragment
+                    return new RegisterStep1();
+                case 1: // Fragment # 0 - This will show FirstFragment different title
+                    return new RegisterStep2();
+                default:
+                    return null;
+            }
+        }
+
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Step " + (position + 1);
+        }
+
+    }
+}
